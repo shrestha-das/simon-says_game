23ff57987a6a1136791b4c711f1791c5ad8dd205
shrestha-das <shresthadas.0.infinity@gmail.com>
2025-11-08 13:33:06 +0530

    updated commit

--- game.js
+++ game.js
@@ -1,12 +1,33 @@
 let gameSeq = [];
 let userSeq = [];
 
+let btns = ["one", "two", "three", "four"];
+
 let started = false;
 let level = 0;
 
-document.addEventListener("keypress", ()=>{
+let h3 = document.querySelector("h3");
+
+document.addEventListener("keypress", function(){
     if(started==false){
         console.log("game is started!");
         started = true;
     }
 });
+
+function btnFlash(btn){
+    btn.classList.add("flash");
+    setTimeout(function (){
+        btn.classList.remove("flash");
+    },1000);
+}
+
+function levelUp(){
+    level++;
+    h3.innerText = `Level ${level}`;
+    
+    let randIndex = Math.floor(Math,random()*3);
+    // let randColor = btns[randIndex];
+    let randBtn = document.querySelector(`.${randIndex+1}`)
+    btnFlash();
+}
